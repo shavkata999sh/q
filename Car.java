80f4aef2fe2fc5e119aac3930641b35e6dfef787
Shavkat <156001276+shavkata999sh@users.noreply.github.com>
2024-01-15 22:49:00 +0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+public class Car {
+//rangi
+//    modeli
+//        narxi
+//    Avtomatyo Mexanik
+
+    public void getrangi(String rangi) {
+System.out.println("rangi");
+    }
+
+    public void getmodel(String modeli) {
+        System.out.println(modeli);
+    }
+
+    public void getnarxi(String narxi) {
+        System.out.println(narxi);
+    }
+    public void getAvtomatyoMexanika(String AvtomatyoMexanika) {
+        System.out.println(AvtomatyoMexanika);
+    }
+}
